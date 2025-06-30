c6aa6d444278b0ddbd0c7d794471e8b91a9fa8b5
Ebubedike <102932168+Ebubedike-ochu@users.noreply.github.com>
2025-06-30 09:54:00 +0100

    Age-Checker

--- main.c
+++ main.c
@@ -1,13 +1,4 @@
-#include "include/input_utils.h"
 #include <stdio.h>
-
-int main() {
-    char name[50];
-    int age = get_int("Enter your age: ");
-    get_string("Enter your name: ");
-    printf("Hello, %s! You are %d years old.\n", name, age);
-    return 0;
-}#include <stdio.h>
 #include <time.h>
 
 int main(void) {
